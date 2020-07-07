14623101b290c5abb6b200842e141dccbf10c77c
PresentKim <debe3721@gmail.com>
2020-07-07 19:20:06 +0900

    Add `declare(strict_types=1);`

--- src/blugin/cropplus/block/NetherWart.php
+++ src/blugin/cropplus/block/NetherWart.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace blugin\cropplus\block;
 
 use blugin\cropplus\CropPlus;
--- src/blugin/cropplus/item/Dye.php
+++ src/blugin/cropplus/item/Dye.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace blugin\cropplus\item;
 
 use pocketmine\item\Item;
